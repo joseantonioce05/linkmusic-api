c01989be12b8d57e33caeebc2dbca8c223ece328
joseantonioce05 <joseantonioce.06@gmail.com>
2024-12-29 20:50:05 -0400

    Inicio del index.js, creado los archivos para los controllers

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+require('dotenv').config();
+
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
+console.log("Bienvenido a LinkMusic")
+
 connection();
+
+const app = express();
+const port = process.env.PORT_EXPRESS;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Rutas
+app.get("/test", (req, res) =>{
+
+    return res.status(200).send("test")
+})
+
+app.listen(port, () => {
+    console.log("El servidor de node esta escuchando en el puerto:", port)
+}) 
